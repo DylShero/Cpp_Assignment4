65777696962dcd1c12714a40e484af7034b92116
DylShero <165214379+DylShero@users.noreply.github.com>
2026-03-25 11:03:31 +0000

    Completed work on move and copy semantics

--- String.h
+++ String.h
@@ -59,6 +59,9 @@ namespace HPC
 
     //Constructor 1 implementation
 String::String(std::size_t in){
+    len = in;
+    capacity = in;
+    data = new char[capacity];
     std::print("Constructing String object of size {} \n", in);
 }
 
@@ -69,7 +72,7 @@ explicit String::String(const char *str){
 
     data = new char[len];
 
-    std::strcpy(data, str);
+    std::memcpy(data, str, len);
 
     std::print("Constructing {} String object of size {}\n", data, len);
 }
@@ -77,22 +80,69 @@ explicit String::String(const char *str){
 //Copy Constructor
 String::String(const String& in){
     std::print("Calling copy constructor\n");
-    String copy = new String(in.data);
+    
+    this->len = in.len; //This is a pointer to the new object being contructed
+    this->capacity = in.capacity
+    this->data = new char[this->len];
+    std::memcpy(this->data, in.data, this->len);
+    
 }
 
 //Move constructor
 String::String(String&& in) noexcept{
     std::print("Calling move contructor\n");
+
+    this->data = in.data;
+    this->data = in.len;
+    this->capacity = in.capacity;
+
+    in.data = nullptr;
+    in.len = 0;
+    in.capacity = 0;
+    
 }
 
 //Copy assign
-String::String& operator=(const String& rhs){
+String& String::operator=(const String& rhs){
     std::print("Calling copy assignment operator\n");
+    if(this == &rhs){
+        print("Trying to copy itself\n");
+        return *this;
+    }
+
+    if(this->capacity < rhs.len){ //Check if enough data memory if not allocate new data memory for input size.
+        delete[] data;
+        this->data = new char[rhs.capacity];
+    }
+
+    this->len = rhs.len;
+    this->capacity = rhs.capacity;
+    std::memcpy(this->data, rhs.data, this->len);
+
+    return *this;
+
 }
 
 //Move assign
-String::String& operator=(String&& rhs) noexcept{
+String& String::operator=(String&& rhs) noexcept{
     std::print("Calling move assignment operator\n");
+
+    if (this == &rhs) {//Check for moving to itself
+        return *this; 
+    }
+
+    delete[] this->data; //Delete existing data in object we want to move data into
+
+    this->capacity = rhs.capacity; //Change to new len and capacity
+    this->len = rhs.len;
+
+    this->data = rhs.data;
+
+    rhs.data = nullptr; //Set to nullptr/0 the old no longer need object.
+    rhs.len = 0;
+    rhs.capacity = 0;
+
+    return *this;
 }
 
 //Destructor
